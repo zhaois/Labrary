575049e283f5e09569c12601f99a68e9abe2a24a
zhaoisme <zhaoisme@yeah.net>
2014-09-30 14:41:36 +0800

    跟新了一些信息

---
+++ Labriry/src/cn/com/itisme/Activities/AtyBorrowingDetail.java
@@ -0,0 +1,13 @@
+package cn.com.itisme.Activities;
+
+import android.app.Activity;
+/**
+ * 
+ * @author John
+ * Information about the details of the borrowing-book
+ *
+ */
+
+public class AtyBorrowingDetail extends Activity {
+
+}
